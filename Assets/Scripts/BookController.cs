e281972d94f00f4ea371af6aa37dbae9b29b6ef9
Ryo Enokizaka <102717173+R-enk@users.noreply.github.com>
2026-07-31 16:01:20 +0900

    一応動作確認済み

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -68,7 +68,7 @@ public class BookController : MonoBehaviour
 
     IEnumerator LoadEpubFromStreamingAssets()
     {
-        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "sapiens.epub");
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "gon.epub");
         UnityWebRequest request = UnityWebRequest.Get(filePath);
         yield return request.SendWebRequest();
 
